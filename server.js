58b6309febcd3f32580d243cca5ad9db26115980
Ryan Lee <ryanm.lee11@gmail.com>
2020-08-20 23:32:41 -0400

    GraphQL schema and local host update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const scheme = require('./schema')
+const schema = require('./schema');
+// const path = require('path');
 
 const app = express();
 
@@ -12,5 +13,11 @@ app.use(
   }),
 );
 
-const PORT = process.env.PORT || 5000
+// app.use(express.static('public'));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+// });
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
